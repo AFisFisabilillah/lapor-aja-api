6cd0c49b07913875125b43527898be1e92383fd4
Afiss <afisfisabilillah21@gmail.com>
2026-02-28 21:12:29 +0700

    add: auditable class for auditing entity

---
+++ src/main/java/com/fizu/laporaja/model/Auditable.java
@@ -0,0 +1,24 @@
+package com.fizu.laporaja.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public abstract class Auditable {
+    @Column(name = "created_ad")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
