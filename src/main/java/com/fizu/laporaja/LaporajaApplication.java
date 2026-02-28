cc31637e69c61fb3ef9d9f2747487f1678b18506
Afiss <afisfisabilillah21@gmail.com>
2026-02-28 16:36:31 +0700

    init project

---
+++ src/main/java/com/fizu/laporaja/LaporajaApplication.java
@@ -0,0 +1,13 @@
+package com.fizu.laporaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaporajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaporajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fizu/laporaja/LaporajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizu.laporaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaporajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
